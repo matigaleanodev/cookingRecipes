1b543974f02a3a62f9501249a2f6dc4f414062d5
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2023-08-17 15:04:43 -0300

    creacion y prueba de pipe para traducir desde Azure API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslatePipe } from './pipes/translate.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslatePipe, FormsModule],
   templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
   title = 'cookingRecipes';
+  originalText = 'Hello, world!';
+  targetLanguage = 'es';
 }
