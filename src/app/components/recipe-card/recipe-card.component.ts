8e08c26983d20b3b8717bf8afa1466cf8fd16529
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2023-08-22 17:09:53 -0300

    creditos, likes, tipos de platos y mas detalles en cards

--- src/app/components/recipe-card/recipe-card.component.ts
+++ src/app/components/recipe-card/recipe-card.component.ts
@@ -2,11 +2,12 @@ import { Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { RecipeInfo } from 'src/app/models/recipe.model';
+import { TooltipDirective } from 'src/app/directives/tooltip.directive';
 
 @Component({
   selector: 'app-recipe-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TooltipDirective],
   templateUrl: './recipe-card.component.html',
   styles: [],
 })
@@ -14,6 +15,7 @@ export class RecipeCardComponent {
   @Input({ required: true }) recipe!: RecipeInfo;
 
   private router = inject(Router);
+
 
   viewRecipeDetail(recipe: RecipeInfo) {
     this.router.navigate([`/recipe/${recipe.id}`]);
--- src/app/directives/tooltip.directive.ts
+++ src/app/directives/tooltip.directive.ts
@@ -2,16 +2,17 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 import { Tooltip } from 'bootstrap';
 @Directive({
   selector: '[Tooltip]',
+  standalone: true
 })
 export class TooltipDirective implements OnInit {
-  @Input() appTooltip: string = '';
+  @Input() tooltip: string = '';
   @Input() placement: any = 'top';
 
   constructor(private el: ElementRef) {}
 
   ngOnInit() {
     new Tooltip(this.el.nativeElement, {
-      title: this.appTooltip,
+      title: this.tooltip,
       placement: this.placement,
     });
   }
